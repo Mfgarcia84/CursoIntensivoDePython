733d120f994145be016d7b362840860585ab1078
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-03 08:29:30 -0300

    update_030222

---
+++ jogo_adivinha_numero.py
@@ -0,0 +1,22 @@
+#Jogo adivinha número de 1 a 10
+from random import randint
+from time import sleep
+continuar = True
+while continuar:
+    computador = randint(1, 10)
+    usuario = int(input("Estou pensando em um número, tente advinhar: "))
+    while True:
+        if computador == usuario:
+            print("Você acertou....Parabéns!!!")
+            break
+        elif computador > usuario:
+            usuario = int(input("Muito baixo...tente um número maior: "))
+        elif computador < usuario:
+            usuario = int(input("Muito alto...tente um número menor: "))
+    continuar = input("Deseja continuar jogando? [s/n] ")
+    if continuar == "s":
+        continuar = True
+    elif continuar == "n":
+        continuar = False
+sleep(1)
+print("Fim do Jogo...")
