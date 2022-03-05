620d08bce0dfb9b482145bc5f44733ceea4c054b
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-04 21:19:26 -0300

    update_040322

---
+++ saudacoes.py
@@ -0,0 +1,4 @@
+nomes = ["raphael", "magno", "wladimir", "abraão", "fábio", "hugo", "renato"]
+for nome in nomes:
+    mensagem = "Vamos assar uma carne amanhã " + nome.title() + "?"
+    print(mensagem)
