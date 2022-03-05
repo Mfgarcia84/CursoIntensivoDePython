98ea9e6d1c953d4ca7d2181b727a8de033f860b8
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-05 07:16:59 -0300

    update_050322

---
+++ lista_de_convidados.py
@@ -0,0 +1,4 @@
+pessoas = ["malena", "andressa", "fernanda"]
+print("Oi " + pessoas[0].title() + ", vamos sair para jantar hoje?")
+print(f"Oi {pessoas[1].title()}, vamos sair para jantar hoje?")
+print("Oi " + pessoas[-1].title() + ", vamos sair para jantar hoje?")
