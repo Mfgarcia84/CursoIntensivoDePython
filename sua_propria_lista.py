b53b0e22c5d57176f675770db90015f1b61594e1
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-04 21:29:11 -0300

    update_040322

---
+++ sua_propria_lista.py
@@ -0,0 +1,13 @@
+MeioDeTransporte = ["uber", "99", "taxi", "bicicleta", "moto", "ônibus"]
+mensagem_1 = "Meu meio de transporte favorito é o " + MeioDeTransporte[0].title() + "."
+print(mensagem_1)
+mensagem_2 = "Quando o " + MeioDeTransporte[0] + " está muito caro eu peço o " + MeioDeTransporte[1] + "."
+print(mensagem_2)
+mensagem_3 = "Eu nunca mais vi um " + MeioDeTransporte[2] + " transitando pela rua."
+print(mensagem_3)
+mensagem_4 = "Eu dei minha " + MeioDeTransporte[-3] + " para meu irmão."
+print(mensagem_4)
+mensagem_5 = "Eu tenho vontade de ter uma " + MeioDeTransporte[-2] + "."
+print(mensagem_5)
+mensagem_6 = "Não ando de " + MeioDeTransporte[-1] + " há mais de 15 anos."
+print(mensagem_6)
