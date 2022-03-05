fc7c2ed3501bc3b01dbedf4cad272d141f05ec63
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-05 07:20:43 -0300

    update_050322

--- lista_de_convidados.py
+++ lista_de_convidados.py
@@ -1,4 +1,16 @@
+#3.4 – Lista de convidados: Se pudesse convidar alguém, vivo ou morto,
+# para o jantar, quem você convidaria? Crie uma lista que inclua pelo
+# menos três pessoas que você gostaria de convidar para jantar.
+# Em seguida, utilize sua lista para exibir uma mensagem para cada pessoa,
+# convidando-a para jantar.
+
+
 pessoas = ["malena", "andressa", "fernanda"]
+
 print("Oi " + pessoas[0].title() + ", vamos sair para jantar hoje?")
 print(f"Oi {pessoas[1].title()}, vamos sair para jantar hoje?")
 print("Oi " + pessoas[-1].title() + ", vamos sair para jantar hoje?")
+
+#outra maneira
+for nome in pessoas:
+    print(f"Oi {nome.title()}, vamos sair para jantar hoje?")
