15cc7092c11dbfe9ad3cf4be4140eb3fa5aa1213
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-08 19:33:12 -0300

    update_080322

---
+++ erro_proposital.py
@@ -0,0 +1,3 @@
+listaDeFerramentas = ["martelo", "alicate", "chace de fenda", "serrote", "nível"]
+print(f"A lista tem {len(listaDeFerramentas)} itens.")
+print(f"O último item da lista é o '{listaDeFerramentas[len(listaDeFerramentas)]}'.")
